3a8860ae4446964b3a2e71cb7bfa848f60430192
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-09 09:35:40 -0400

    Comment out install of k4atools and libsoundio1

--- scripts/install-azure-kinect-drivers.sh
+++ scripts/install-azure-kinect-drivers.sh
@@ -9,8 +9,8 @@ curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1
 curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/kinect/libk4a1.4-dev_1.4.1_amd64.deb
 curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4abt1.1/libk4abt1.1_1.1.2_amd64.deb > /tmp/kinect/libk4abt1.1_1.1.2_amd64.deb
 curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4abt1.1-dev/libk4abt1.1-dev_1.1.2_amd64.deb > /tmp/kinect/libk4abt1.1-dev_1.1.2_amd64.deb
-curl -SL http://ftp.de.debian.org/debian/pool/main/libs/libsoundio/libsoundio1_1.1.0-1_amd64.deb > /tmp/kinect/libsoundio1_1.1.0-1_amd64.deb
-curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/k/k4a-tools/k4a-tools_1.4.1_amd64.deb > /tmp/kinect/k4a-tools_1.4.1_amd64.deb
+# curl -SL http://ftp.de.debian.org/debian/pool/main/libs/libsoundio/libsoundio1_1.1.0-1_amd64.deb > /tmp/kinect/libsoundio1_1.1.0-1_amd64.deb
+# curl -SL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/k/k4a-tools/k4a-tools_1.4.1_amd64.deb > /tmp/kinect/k4a-tools_1.4.1_amd64.deb
 
 # Automatically accept EULA; used for automated installs in Docker containers and GitHub Workflows
 echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | debconf-set-selections
@@ -23,9 +23,9 @@ dpkg -i /tmp/kinect/libk4abt1.1_1.1.2_amd64.deb
 dpkg -i /tmp/kinect/libk4abt1.1-dev_1.1.2_amd64.deb
 
 # libsoundio1 is a dependency of k4a-tools
-dpkg -i /tmp/kinect/libsoundio1_1.1.0-1_amd64.deb
+# dpkg -i /tmp/kinect/libsoundio1_1.1.0-1_amd64.deb
 
-dpkg -i /tmp/kinect/k4a-tools_1.4.1_amd64.deb
+# dpkg -i /tmp/kinect/k4a-tools_1.4.1_amd64.deb
 
 # Remove temporary folder
 rm -rf tmp/kinect
