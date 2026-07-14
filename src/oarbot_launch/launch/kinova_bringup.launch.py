9349443b9afa0e8fb756e8c28a5a0c9be63e307d
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-14 11:21:07 -0400

    Rework kinova bringup to directly use kinova driver node and not the launch file

--- src/oarbot_launch/launch/kinova_bringup.launch.py
+++ src/oarbot_launch/launch/kinova_bringup.launch.py
@@ -1,39 +1,42 @@
+import os
+import yaml
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import PushROSNamespace
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
-from launch.actions import IncludeLaunchDescription, GroupAction, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def yaml_to_dict(path_to_yaml: str):
+    with open(path_to_yaml, "r") as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
 
 def generate_launch_description() -> LaunchDescription:
-    kinova_namespace_string = "namespace"
-    kinova_namespace_argument = DeclareLaunchArgument(kinova_namespace_string, description="Namespace used by the kinova robot; should be unique")
-
-    frame_prefix_string = "frame_prefix"
-    frame_prefix_argument = DeclareLaunchArgument(frame_prefix_string,description="Prefix for tf frames from the Kinova arm; should be unique and include a trailing forward slash if not empty")
-
-    kinova_robot_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("kinova_bringup"),
-                "launch",
-                "kinova_robot_launch.py"
-            ])
-        ),
-        launch_arguments=[
-            (frame_prefix_string, LaunchConfiguration(frame_prefix_string))
-        ]
-    )
-
-    namespace_group = GroupAction(
-        actions=[
-            PushROSNamespace(LaunchConfiguration(kinova_namespace_string)),
-            kinova_robot_launch
-        ]
-    )
+    """
+    Launches the Kinova Jaco2 driver node under the given namespace
+    """
     
+    kinova_namespace_string = "kinova_namespace"
+    kinova_namespace_argument = DeclareLaunchArgument(kinova_namespace_string, description="Namespace all topics and nodes for the Kinova arm are published under")
+
+    kinova_yaml_params = yaml_to_dict(os.path.join(get_package_share_directory("kinova_bringup"), "launch/config", "robot_parameters.yaml"))
+    
+    configurable_parameters = [
+        {"name": "use_urdf",              "default": "true"},
+        {"name": "kinova_robotType",      "default": "j2n6s300"},
+        {"name": "kinova_robotSerial",    "default": "not_set"},
+        {"name": "use_jaco_v1_fingers",   "default": True},
+        {"name": "feedback_publish_rate", "default": 0.1},
+        {"name": "tolerance",             "default": 2.0},
+    ]
+
     return LaunchDescription([
-        kinova_namespace_argument,
-        frame_prefix_argument,
-        namespace_group
+            kinova_namespace_argument,
+            Node(
+                package="kinova_driver",
+                executable="kinova_arm_driver",
+                parameters=[dict([(param["name"], param["default"]) for param in configurable_parameters]), kinova_yaml_params],
+                output="screen",
+                namespace=LaunchConfiguration(kinova_namespace_string)
+            )
     ])
