89801db7ae07e1dd738281738f04361ce1500239
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-20 10:35:28 -0400

    Add display env variable set to bash script

--- docs/installation/oarbot_silver_helper_laptop_launch.sh
+++ docs/installation/oarbot_silver_helper_laptop_launch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# In order for the Azure Kinect node to launch, a display needs to be connected. This can be done by SSH'ing
+# with -X, but we will use the laptop's built-in display variable to launch the node
+export DISPLAY=:1
+
 # Source the global ROS2 installation
 source /opt/ros/jazzy/setup.bash
 
