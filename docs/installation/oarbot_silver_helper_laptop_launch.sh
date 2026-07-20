b7e014adebea7177b72cda7333ae7b6f718fa6e1
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-20 10:48:45 -0400

    Add fail on error

--- docs/installation/oarbot_silver_helper_laptop_launch.sh
+++ docs/installation/oarbot_silver_helper_laptop_launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # In order for the Azure Kinect node to launch, a display needs to be connected. This can be done by SSH'ing
 # with -X, but we will use the laptop's built-in display variable to launch the node
