94e834be3d76e43fba7d70bed66a4cc74d87ca8e
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-25 17:43:24 -0400

    Switch to launching the node directly instead of passing into another launch file

--- src/oarbot_launch/launch/oarbot_silver_moveit.launch.py
+++ src/oarbot_launch/launch/oarbot_silver_moveit.launch.py
@@ -2,23 +2,22 @@ from launch import LaunchDescription
 from launch.actions import GroupAction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
-    moveit_launch = PathJoinSubstitution([
-        FindPackageShare("oarbot_silver_moveit"),
-        "launch",
-        "move_group.launch.py",
-    ])
+    moveit_config = MoveItConfigsBuilder("oarbot_silver", package_name="oarbot_silver_moveit").to_moveit_configs()
 
     return LaunchDescription([
-        GroupAction([
-            PushRosNamespace("oarbot_silver"),
-
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(moveit_launch)
-            ),
-        ])
+        Node(
+            package="moveit_ros_move_group",
+            executable="move_group",
+            output="screen",
+            namespace="oarbot_silver",
+            parameters=[
+                moveit_config.to_dict()
+            ]
+        )
     ])
