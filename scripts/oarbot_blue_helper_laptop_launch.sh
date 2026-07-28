3c6e09a3e7b78a762103eb0d5a8b9a44f4e8a245
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-28 12:03:37 -0400

    Switch to using NVIDIA gpu for xorg virtual display

--- scripts/oarbot_blue_helper_laptop_launch.sh
+++ scripts/oarbot_blue_helper_laptop_launch.sh
@@ -2,8 +2,15 @@
 set -e
 
 # In order for the Azure Kinect node to launch, a display needs to be connected. This can be done by SSH'ing
-# with -X, but we will use a virtual display
+# with -X, but we will use a virtual display backed by the NVIDIA GPU
 export DISPLAY=:2
+
+# Force GLX/EGL to use the NVIDIA vendor library rather than Mesa/llvmpipe.
+# This matters on laptops with hybrid Intel+NVIDIA graphics, where glvnd
+# could otherwise pick the wrong vendor.
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __VK_LAYER_NV_optimus=NVIDIA_only
 
 # Source the global ROS2 installation
 source /opt/ros/jazzy/setup.bash
