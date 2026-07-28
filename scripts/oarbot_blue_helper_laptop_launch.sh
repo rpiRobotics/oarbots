9782bedd1acd6176f13b734ae9948a1090510853
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-07-28 10:52:07 -0400

    Add oarbot blue bash and service scripts

---
+++ scripts/oarbot_blue_helper_laptop_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# In order for the Azure Kinect node to launch, a display needs to be connected. This can be done by SSH'ing
+# with -X, but we will use a virtual display
+export DISPLAY=:2
+
+# Source the global ROS2 installation
+source /opt/ros/jazzy/setup.bash
+
+# Use the correct DDS provider
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp 
+
+# Source your specific workspace setup file
+source ~/ros2_ws/install/local_setup.bash
+
+# Run your target launch file
+ros2 launch oarbot_launch oarbot_blue_helper_laptop.launch.py
