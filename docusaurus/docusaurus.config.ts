1d366cf036f92a3e3632e3dca7df478cbfb24315
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-04 16:08:36 -0400

    Fix edit path

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/rpiRobotics/oarbots",
+            "https://github.com/rpiRobotics/oarbots/docusaurus",
         },
         theme: {
           customCss: "./src/css/custom.css",
