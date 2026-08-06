2b9e9464f34569205f5c3f91d79c5d75d5cf7463
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-06 10:37:31 -0400

    Update nuc_launch to use python venv

--- scripts/nuc_launch.sh
+++ scripts/nuc_launch.sh
@@ -11,8 +11,9 @@ source /opt/ros/jazzy/setup.bash
 # Use the correct DDS provider
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp 
 
-# Source your specific workspace setup file
+# Source the ROS2 workspace and Python Virtual Environment
 source ~/ros2_ws/install/local_setup.bash
+source ~/ros2_ws/.venv/bin/activate
 
 # Run your target launch file
 ros2 launch oarbot_launch nuc.launch.py
