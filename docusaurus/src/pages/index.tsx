c4f4630de6cde7a865ec2d32350b1ba8e826499f
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-07 17:22:09 -0400

    Add draft of OARBots homepage

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -1,44 +1,52 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+export default function Home() {
+    const { siteConfig } = useDocusaurusContext();
+    
+    return (
+        <Layout>
+            <main>
+                <div className="flex flex-col justify-center items-center w-screen py-24 bg-gray-100 dark:bg-gray-800">
+                    <h1 className="text-5xl">OARBots Documentation</h1>
+                    <span className="h-2"></span>
+                    <Link className="hover:no-underline text-2xl bg-blue-300 dark:bg-blue-900 hover:bg-blue-400 hover:dark:bg-blue-800 px-6 py-2 text-black dark:text-white rounded-xl font-semibold border-1 border-blue-200 dark:border-blue-800 transition hover:-translate-y-0.5 hover:drop-shadow-md" to="/docs/getting-started">
+                        View the Docs
+                    </Link>
+                </div>
+                <div className="flex flex-col py-6 px-12">
+                    <p className="font-medium text-md ">
+                        Rensselaer Polytechnic Institute's Center for Smart Convergent Manufacturing Systems (CSCMS)
+                        developed and built two omni-directional assistive robots, or OARBots, for use in various
+                        research projects and industrial applications. The OARBot system runs ROS2 and consists of
+                        sensors and external computers in addition to the OARBots themselves. This site serves
+                        to:
+                    </p>
+                    <ul>
+                        <li>
+                            Document their physical makeup, including parts, electronics, and sensors
+                        </li>
+                        <li>
+                            Provide usage instructions to others
+                        </li>
+                        <li>
+                            Document common troubleshooting steps
+                        </li>
+                        <li>
+                            Explain the inner-workings of the OARBot code for future maintainers
+                        </li>
+                    </ul>
+                    <p className="font-medium text-md ">
+                        If you find errors or would like to add to the documentation, submit a pull request to
+                        the <a href="https://github.com/rpiRobotics/oarbots">OARBots GitHub</a>. Under each doc, there
+                        is an "edit this page" link which will take you right to the file that generated the doc. Our
+                        docs are written in <a href="https://mdxjs.com/">MDX</a>, which supports
+                        standard <a href="https://www.markdownguide.org/">Markdown</a> syntax with additional HTML/JS
+                        support.
+                    </p>
+                </div>
+            </main>
+        </Layout>
+    );
 }
