2edf87458290410c31b68a3c41a31fd135a05c1b
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-07 20:36:23 -0400

    Update for github pages deployment

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -15,13 +15,13 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://rpiRobotics.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often "/<projectName>/"
-  baseUrl: "/",
-
+  baseUrl: "/oarbots/",
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: "rpiRobotics", // Usually your GitHub org/user name.
   projectName: "oarbots", // Usually your repo name.
 
