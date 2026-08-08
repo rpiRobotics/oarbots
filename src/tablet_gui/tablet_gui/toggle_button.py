4cc84dbca494f5b5b629f2c04f40299834e34486
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-08 16:40:20 -0400

    Add custom toggled color

--- src/tablet_gui/tablet_gui/toggle_button.py
+++ src/tablet_gui/tablet_gui/toggle_button.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import (
     QPushButton
 )
-from PySide6.QtGui import QColor, QPainter, QFont
+from PySide6.QtGui import QColor, QPainter, QFont, QPalette
 from PySide6.QtCore import Qt
 
 class ToggleButton(QPushButton):
@@ -11,9 +11,23 @@ class ToggleButton(QPushButton):
         self.setCheckable(True)
         self.clicked.connect(on_click)
 
+        # Handle color changes for clicking the button; we want a more pronounced color change than the default
+        self.default_color = self.palette().button()
+        self.clicked.connect(self.update_color)
+
         self.setFont(QFont("Roboto", 16))
         self.setStyleSheet("""
             QPushButton {
                 padding: 16px 0px;
             }
         """)
+
+    def update_color(self, toggled: bool) -> None:
+        palette = self.palette()
+
+        if toggled:
+            palette.setColor(QPalette.ColorRole.Button, QColor("#5566EE"))
+        else:
+            palette.setBrush(QPalette.ColorRole.Button, self.default_color)
+
+        self.setPalette(palette)
