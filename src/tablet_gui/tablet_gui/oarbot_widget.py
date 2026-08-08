49286f261c169de0313964efa7bfcc44a96bd8de
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-08 16:17:51 -0400

    Add basic toggle and push buttons

--- src/tablet_gui/tablet_gui/oarbot_widget.py
+++ src/tablet_gui/tablet_gui/oarbot_widget.py
@@ -8,6 +8,8 @@ from PySide6.QtCore import Qt
 
 from tablet_gui.ros_gui_node import RosGuiNode
 from tablet_gui.oarbot_status import OarbotStatus
+from tablet_gui.push_button import PushButton
+from tablet_gui.toggle_button import ToggleButton
 class OarbotWidget(QWidget):
     def __init__(self, oarbot_namespace: str, display_name: str, ros_gui_node: RosGuiNode) -> None:
         super().__init__()
@@ -21,6 +23,7 @@ class OarbotWidget(QWidget):
         layout.setContentsMargins(24, 24, 24, 24)
 
         self.oarbot_main_text = QLabel(self.display_name)
+        self.oarbot_main_text.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.oarbot_main_text.setFont(QFont("Roboto", 24))
         layout.addWidget(self.oarbot_main_text)
 
@@ -33,6 +36,12 @@ class OarbotWidget(QWidget):
             },
             ros_gui_node=self.ros_gui_node
         )
-        
-
         layout.addWidget(self.oarbot_status)
+
+        self.arm_enabled = False
+        self.arm_enable_button = ToggleButton("Arm Enabled" if self.arm_enabled else "Arm Disabled", self.handle_arm_enable_button)
+        layout.addWidget(self.arm_enable_button)
+
+    def handle_arm_enable_button(self, checked: bool) -> None:
+        self.arm_enabled = checked
+        self.arm_enable_button.setText("Arm Enabled" if checked else "Arm Disabled")
---
+++ src/tablet_gui/tablet_gui/toggle_button.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import (
+    QPushButton
+)
+from PySide6.QtGui import QColor, QPainter, QFont
+from PySide6.QtCore import Qt
+
+class ToggleButton(QPushButton):
+    def __init__(self, text: str, on_click) -> None:
+        super().__init__(text)
+
+        self.setCheckable(True)
+        self.clicked.connect(on_click)
+
+        self.setFont(QFont("Roboto", 16))
+        self.setStyleSheet("""
+            QPushButton {
+                padding: 16px 64px;
+            }
+        """)
