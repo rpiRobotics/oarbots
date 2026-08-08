0e7dbd43c155d78ceb1c1cee6f56048d0e7daef5
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-08 15:41:51 -0400

    Add status indicators

--- src/tablet_gui/tablet_gui/oarbot_widget.py
+++ src/tablet_gui/tablet_gui/oarbot_widget.py
@@ -1,6 +1,13 @@
-from PySide6.QtWidgets import QWidget
-from tablet_gui.ros_gui_node import RosGuiNode
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel
+)
+from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
 
+from tablet_gui.ros_gui_node import RosGuiNode
+from tablet_gui.oarbot_status import OarbotStatus
 class OarbotWidget(QWidget):
     def __init__(self, oarbot_namespace: str, display_name: str, ros_gui_node: RosGuiNode) -> None:
         super().__init__()
@@ -8,3 +15,24 @@ class OarbotWidget(QWidget):
         self.oarbot_namespace = oarbot_namespace
         self.display_name = display_name
         self.ros_gui_node = ros_gui_node
+
+        layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        layout.setContentsMargins(32, 32, 32, 32)
+
+        self.oarbot_main_text = QLabel(self.display_name)
+        self.oarbot_main_text.setFont(QFont("Roboto", 36))
+        layout.addWidget(self.oarbot_main_text)
+
+        self.oarbot_status = OarbotStatus(
+            display_name_to_node_name={
+                "Dingo Base": "dingo",
+                "Kinova Arm": "kinova",
+                "Force Torque": "ft",
+                "MoveIt": "moveit"
+            },
+            ros_gui_node=self.ros_gui_node
+        )
+        
+
+        layout.addWidget(self.oarbot_status)
--- src/tablet_gui/tablet_gui/ros_gui_node.py
+++ src/tablet_gui/tablet_gui/ros_gui_node.py
@@ -5,3 +5,6 @@ class RosGuiNode(Node):
         super().__init__("ros_gui_node")
 
         self.get_logger().info("Started ROS2 Node")
+
+    def is_node_active(self, node_name: str) -> bool:
+        return node_name in self.get_fully_qualified_node_names()
