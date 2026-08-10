4c1f97ad78d5d5fb1c4dc22d04f022a758675885
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-10 10:23:01 -0400

    Read data from the csv

--- src/force_torque_calibration/force_torque_calibration/force_torque_calibration_node.py
+++ src/force_torque_calibration/force_torque_calibration/force_torque_calibration_node.py
@@ -1,9 +1,22 @@
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor
+import csv
 
 class ForceTorqueCalibration(Node):
     def __init__(self) -> None:
         super().__init__("force_torque_calibration")
 
+        joint_states_csv_description = ParameterDescriptor(description="Path to the csv file containing rows of 6 numbers, describing the joint states of the Kinova arm")
+        self.declare_parameter("joint_states_csv", value="", descriptor=joint_states_csv_description)
+        joint_states_csv_path = self.get_parameter("joint_states_csv").get_parameter_value().string_value
+        if joint_states_csv_path == "":
+            raise ValueError("joint_states_csv is empty")
+
+        self.joint_states_list: list[list[float]] = []
+        with open(joint_states_csv_path, newline="") as file:
+            data = csv.reader(file)
+            for row in data:
+                self.joint_states_list.append([float(num) for num in row])
 
         
--- src/force_torque_calibration/force_torque_calibration/main.py
+++ src/force_torque_calibration/force_torque_calibration/main.py
@@ -6,6 +6,3 @@ def main(args=None) -> None:
     rclpy.init(args=args)
 
     force_torque_calibration = ForceTorqueCalibration()
-
-    rclpy.spin(force_torque_calibration)
-    rclpy.shutdown()
