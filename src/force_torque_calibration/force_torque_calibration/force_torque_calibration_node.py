e4e8c40358b55b934f6278aca09e58b2eac5d520
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-10 12:14:27 -0400

    Added steps for collecting data

--- src/force_torque_calibration/force_torque_calibration/force_torque_calibration_node.py
+++ src/force_torque_calibration/force_torque_calibration/force_torque_calibration_node.py
@@ -1,11 +1,17 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
+from rclpy.qos import qos_profile_sensor_data
+from rclpy.time import Time
 from rcl_interfaces.msg import ParameterDescriptor
 import csv
 from kinova_msgs.action import ArmJointAngles
 from kinova_msgs.msg import JointAngles
+from geometry_msgs.msg import WrenchStamped, Vector3Stamped
 from math import pi
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+import tf2_geometry_msgs
 
 class ForceTorqueCalibration(Node):
     def __init__(self) -> None:
@@ -16,6 +22,14 @@ class ForceTorqueCalibration(Node):
         joint_angles_csv_path = self.get_parameter("joint_angles_csv").get_parameter_value().string_value
         if joint_angles_csv_path == "":
             raise ValueError("joint_angles_csv is empty")
+
+        tf_prefix_description = ParameterDescriptor(description="Prefix for tf frames; should include a trailing slash")
+        self.declare_parameter("tf_prefix", value="/", descriptor=tf_prefix_description)
+        self.tf_prefix = self.get_parameter("tf_prefix").get_parameter_value().string_value
+        if self.tf_prefix == "":
+            raise ValueError("If you want no tf prefix, leave the paramter blank or set it to '/'")
+        if self.tf_prefix == "/":
+            self.get_logger().warn("tf prefix is '/'; continuing...")
 
         # Get the data from the CSV into a list
         self.joint_angles_list: list[JointAngles] = []
@@ -39,13 +53,16 @@ class ForceTorqueCalibration(Node):
             self.get_logger().error("Kinova joint angle action server is not available")
             raise RuntimeError("Kinova joint angle action server is not available")
 
+
+        # Initialize the tf listener
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
     def move_arm_to_state(self, joint_angles: JointAngles) -> None:
-        self.get_logger().info(f"Starting to move to {joint_angles}")
         goal_msg = ArmJointAngles.Goal()
         goal_msg.angles = joint_angles
 
         future = self.joint_angles_action.send_goal_async(goal_msg)
-        self.get_logger().info("Sending action goal")
         rclpy.spin_until_future_complete(self, future)
 
         goal_handle = future.result()
@@ -55,4 +72,52 @@ class ForceTorqueCalibration(Node):
 
         result_future = goal_handle.get_result_async()
         rclpy.spin_until_future_complete(self, result_future)
-        self.get_logger().info("Action completed")
+
+    def collect_force_torque_data(self) -> None:
+        self.force_torque_data: list[WrenchStamped] = []
+        self.force_torque_subscription = self.create_subscription(
+            msg_type=WrenchStamped,
+            topic="force_torque_raw",
+            callback=self.handle_force_torque_data,
+            qos_profile=qos_profile_sensor_data
+        )
+
+        # Collect data until we have enough (the subscription is stopped in the callback)
+        while len(self.force_torque_data) < 100:
+            rclpy.spin_once(self)
+
+    def stop_force_torque_subscription(self) -> None:
+        if hasattr(self, "force_torque_subscription"):
+            self.destroy_subscription(self.force_torque_subscription)
+
+    def handle_force_torque_data(self, msg: WrenchStamped) -> None:
+        self.force_torque_data.append(msg)
+
+        if len(self.force_torque_data) == 100:
+            self.stop_force_torque_subscription()
+
+    def get_gravity_vector(self) -> Vector3Stamped:
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                source_frame=self.tf_prefix + "base_link",
+                target_frame=self.tf_prefix + "j2n6s300_ft_robot_side_connector",
+                time=Time()
+            )
+
+            gravity_vector_base_link = Vector3Stamped()
+            gravity_vector_base_link.header.frame_id = self.tf_prefix + "base_link"
+            gravity_vector_base_link.vector.x = 0.0
+            gravity_vector_base_link.vector.y = 0.0
+            gravity_vector_base_link.vector.z = -9.80665
+
+            gravity_vector_force_torque = tf2_geometry_msgs.do_transform_vector3(gravity_vector_base_link, transform)
+
+            return gravity_vector_force_torque
+
+
+
+        except Exception as ex:
+            self.get_logger().error(f"Could not get the transformation from {self.tf_prefix + "base_link"} to {self.tf_prefix + "j2n6s300_ft_robot_side_connector"}")
+            raise LookupError(f"Could not get the transformation from {self.tf_prefix + "base_link"} to {self.tf_prefix + "j2n6s300_ft_robot_side_connector"}")
+
+    
--- src/force_torque_calibration/force_torque_calibration/main.py
+++ src/force_torque_calibration/force_torque_calibration/main.py
@@ -1,11 +1,41 @@
 import rclpy
+import time
+from geometry_msgs.msg import Vector3Stamped
 
 from force_torque_calibration.force_torque_calibration_node import ForceTorqueCalibration
 
 def main(args=None) -> None:
     rclpy.init(args=args)
 
-    force_torque_calibration = ForceTorqueCalibration()
+    print("-----------WARNING-----------")
+    print("Unplug the Azure Kinect camera (this requires a restart of the node after running this calibration) and remove the both the Azure Kinect and force torque cables from the Zip-Tie")
+    input("Press Enter to continue...")
 
-    for joint_angles in force_torque_calibration.joint_angles_list:
-        force_torque_calibration.move_arm_to_state(joint_angles)
+    node = ForceTorqueCalibration()
+
+    for i, joint_angles in enumerate(node.joint_angles_list):
+        node.get_logger().info(f"Moving arm to position #{i}")
+        node.move_arm_to_state(joint_angles)
+
+        node.get_logger().info("Waiting for arm to finish shaking")
+        time.sleep(5)
+
+        node.get_logger().info("Collecting force torque data")
+        node.collect_force_torque_data()
+
+        node.get_logger().info("Getting gravity in force torque frame")
+        gravity_vector = Vector3Stamped()
+        for i in range(10):
+            try:
+                gravity_vector = node.get_gravity_vector()
+            except LookupError as ex:
+                node.get_logger().info("Trying to get transform again")
+                time.sleep(0.5)
+                continue
+
+            break
+
+        if gravity_vector == Vector3Stamped():
+            raise LookupError("Failed to get transform 10 times; exiting")
+
+        node.get_logger().info("")
