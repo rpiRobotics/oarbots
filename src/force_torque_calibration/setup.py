4126698add464f4966db17e41f29537500be444c
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-08-11 10:15:44 -0400

    Add launch file

--- src/force_torque_calibration/setup.py
+++ src/force_torque_calibration/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'force_torque_calibration'
 
@@ -9,7 +11,9 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'resource'), glob(os.path.join('resource', '*.csv'))),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools', "numpy"],
     zip_safe=True,
